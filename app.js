a3ccc1b7ae41c3c6efb1d5de1567a0cc688aeb86
Sean Bradley <sb_210@hotmail.com>
2023-10-13 17:59:11 -0400

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // require - looks for the rectable.js file and takes the exports from that file and set them as properties or methods of this rect object.
+// rect - calls the function from rectangle.js which checks if l,w are positive numbers, if not it returns the callback function with an err object as it's first argument. expects 3 values: 2 positive numbers and a callback function.
+// err, rectangle - this arrow (callback) function is not being executed here in the parameter list, only being defined. It won't be used until it enter the code inside the rectangle module (rectangle.js).
+// if (err) - if the function in rectangle.js returned an error, then console log the error message.
+// if (err) returned false (the function in rectangle.js did not error) then the else statment kicks and we know the callback was called with a first argument of null.
+// rectangle.area - the object that owns these methods is now being passed into this callback function with the name rectangle as defined in the paramter list of the rect function.
 
 const rect = require('./rectangle');
 
